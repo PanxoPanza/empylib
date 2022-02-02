230f1e6ef6ca2a21ba1a39816cba07deb874c56a
PanxoPanza <framirez.cuevas@gmail.com>
2022-02-02 13:56:46 -0300

    updated setup.py

--- ref_spectra/__init__.py
+++ ref_spectra/__init__.py
@@ -1,2 +1,2 @@
 
-from .ref_spectra import *
+from .ref_spectra import AM15, T_atmosphere, Plank_BB
--- setup.py
+++ setup.py
@@ -6,9 +6,14 @@ setup(
       description='Standard python library for computational electromagnetics',
       author='Francisco V. Ramirez-Cuevas',
       author_author_email='francisco.ramirez.c@uai.cl',
-      url='',
+      url='https://github.com/PanxoPanza/empylib.git',
       packages=find_packages(exclude=()),
       install_requires=[
           'numpy', 'os', 'scipy'
-          ]
+          ],
+      classifiers = [
+      "Programming Language :: Python :: 3",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+      ],
       )
