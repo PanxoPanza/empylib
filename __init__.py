a5f780cafcd1bb12e695d83728255f1804f593ab
PanxoPanza <framirez.cuevas@gmail.com>
2023-08-04 17:53:44 -0400

    added version to init

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ from . import waveoptics
 from . import rad_transfer
 
 __version__ = "0.1.2"
-__author__ = 'Francisco Ramirez'
+__author__ = 'Francisco V. Ramirez-Cuevas'
 __credits__ = 'Universidad Adolfo Ibañez'
 
 # standard constants
